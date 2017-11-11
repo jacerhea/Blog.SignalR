707fd47474ae6708ce30d1c32ddcac02639a5b6b
Jace <jace.rhea@gmail.com>
2017-11-11 16:40:36 -0600

    Updating nuget packages.

--- Blog.SignalRExample/Scripts/_references.js
+++ Blog.SignalRExample/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.1.1.js" />
+﻿/// <reference path="jquery-3.2.1.js" />
 /// <reference path="jquery-ui-1.8.20.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
